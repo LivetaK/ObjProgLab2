d0f6804d73ae5cfa02ee4787d9d4324cfdaac7dc
LivetaK <159531709+LivetaK@users.noreply.github.com>
2024-05-13 22:22:57 +0300

    Update studentai.h

--- studentai.h
+++ studentai.h
@@ -11,129 +11,54 @@
 #include <string>
 #include <chrono>
 #include <cstdlib>
+#include <cassert>
 
 using namespace std;
 using namespace std::chrono;
 
 class mok {
 private:
-    vector<int> nd = {0};
     string var = " ", pav = " ";
     int eg = 0;
+    vector<int> nd = { 0 };
     double gal_vid = 0.0 , gal_med = 0.0;
 public:
-    void setvar(string& vardas) {
-        var = vardas;
-    }
-    string getvar() const {
-        return var;
-    }
-    void setpav(string& pavarde) {
-        pav = pavarde;
-    }
-    string getpav() const {
-        return pav;
-    }
-    void seteg(int egzaminas) {
-        eg = egzaminas;
-    }
-    int geteg()const {
-        return eg;
-    }
-    void setgal_vid(double Gal_vid) {
-        gal_vid = Gal_vid;
-    }
-    double getgal_vid() const {
-        return gal_vid;
-    }
-    void setgal_med(double Gal_med) {
-        gal_med = Gal_med;
-    }
-    double getgal_med() const {
-        return gal_med;
-    }
-    void setviennd(int naujasnd) {
-        nd.push_back(naujasnd);
-    }
-    int getviennd(const vector<int>& ND, int i)const {
-        return ND[i];
-    }
-    void setnd(const vector<int>& ND) {
-        nd = ND;
-    }
-    vector<int> getnd() const{
-        return nd;
-    }
-    void isvalymas() {
-        nd.clear();
-        var.clear();
-        pav.clear();
-        eg = 0;
-        gal_vid = 0;
-        gal_med = 0;
-    }
+    void setvar(const string& vardas);
+    string getvar() const;
+    void setpav(const string& pavarde);
+    string getpav() const;
+    void seteg(int egzaminas);
+    int geteg()const;
+    void setgal_vid(double Gal_vid);
+    double getgal_vid() const;
+    void setgal_med(double Gal_med);
+    double getgal_med() const;
+    void setnd(const vector<int>& ND);
+    vector<int> getnd() const;
+  
+    void isvalymas();
     // RULE OF FIVE:
-
+    // Constructor
     mok() = default;
 
     // Destructor
-    ~mok() {
-        nd.clear();
-    }
+    ~mok();
 
     // Copy Constructor
-    mok(const mok& laikStud) {
-        var = laikStud.var;
-        pav = laikStud.pav;
-        eg = laikStud.eg;
-        gal_vid = laikStud.gal_vid;
-        gal_med = laikStud.gal_med;
-        nd = laikStud.nd;
-    }
+    mok(const mok& laikStud);
 
     // Copy Assignment Operator
-    mok& operator=(const mok& laikStud) {
-        if (this != &laikStud) {
-            var = laikStud.var;
-            pav = laikStud.pav;
-            eg = laikStud.eg;
-            gal_vid = laikStud.gal_vid;
-            gal_med = laikStud.gal_med;
-            nd = laikStud.nd;
-        }
-        return *this;
-    }
+    mok& operator=(const mok& laikStud);
 
     // Move Constructor
-    mok(mok&& laikStud) noexcept {
-        var = move(laikStud.var);
-        pav = move(laikStud.pav);
-        eg = laikStud.eg;
-        gal_vid = laikStud.gal_vid;
-        gal_med = laikStud.gal_med;
-        nd = move(laikStud.nd);
-        // Reset the moved object
-        laikStud.eg = 0;
-        laikStud.gal_vid = 0.0;
-        laikStud.gal_med = 0.0;
-    }
+    mok(mok&& laikStud) noexcept;
 
     // Move Assignment Operator
-    mok& operator=(mok&& laikStud) noexcept {
-        if (this != &laikStud) {
-            var = move(laikStud.var);
-            pav = move(laikStud.pav);
-            eg = laikStud.eg;
-            gal_vid = laikStud.gal_vid;
-            gal_med = laikStud.gal_med;
-            nd = move(laikStud.nd);
-            // Reset the moved object
-            laikStud.eg = 0;
-            laikStud.gal_vid = 0.0;
-            laikStud.gal_med = 0.0;
-        }
-        return *this;
-    }
+    mok& operator=(mok&& laikStud) noexcept;
+
+    friend ostream& operator<< (ostream& output, const mok& stud);
+
+    friend istream& operator>>(istream& input, mok& stud);
 
 };
 
@@ -155,3 +80,4 @@ void failuNuskaitymas(vector<mok>& studentai, string& failoPavadinimas);
 int pirmasP(int& pirmasPasirinkimas);
 int treciasP(int& treciasPasirinkimas);
 void rikiavimas(const int ketvirtaspasirinkimas, vector<mok>& studentai);
+void testavimoRezultatai(bool result, const string& testas);
